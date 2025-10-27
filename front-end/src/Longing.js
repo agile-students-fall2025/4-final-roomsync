30927e6a71ce3b63a223366e725c4f4581224c17
eselmsenavarank <varankeslemsena@gmail.com>
2025-10-27 02:48:58 -0400

    added page implementation of longing (not app.js)

---
+++ front-end/src/Longing.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+export default function Longing() {
+
+  
+
+  return (
+    <>
+     <div class="">
+    <form action="get">
+     <h3>Upcoming Chorses</h3>
+     <ul id="chorses"></ul>
+     <h3>Upcoming Payments</h3>
+     <ul id="payments"></ul>
+    </form>
+    
+     <h3>Living with roommates can be a rewarding experience, but it often comes with significant friction. RoomSync is here for you:</h2>
+     <ul>
+        <li>Chore and expense tracking</li>
+        <li>Finding a compatible roommate</li>
+        <li>Building a genuine community</li>
+     </ul>
+    </div>
+    <a href="default.asp">Sign In</a>
+    <a href="default.asp">Register</a>
+    </>
+  )
+}
+
+
