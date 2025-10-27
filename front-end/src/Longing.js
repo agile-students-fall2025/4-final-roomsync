5411bebcc4178ef2a802135d7c738aaefabe484c
eselmsenavarank <varankeslemsena@gmail.com>
2025-10-27 03:01:44 -0400

    created longing page (not app.js)

--- front-end/src/Longing.js
+++ front-end/src/Longing.js
@@ -13,16 +13,12 @@ export default function Longing() {
      <h3>Upcoming Payments</h3>
      <ul id="payments"></ul>
     </form>
-    
-     <h3>Living with roommates can be a rewarding experience, but it often comes with significant friction. RoomSync is here for you:</h2>
-     <ul>
-        <li>Chore and expense tracking</li>
-        <li>Finding a compatible roommate</li>
-        <li>Building a genuine community</li>
-     </ul>
     </div>
-    <a href="default.asp">Sign In</a>
-    <a href="default.asp">Register</a>
+
+    <a href="default.asp">Skill Swap</a>
+    <a href="default.asp">Chores</a>
+    <a href="default.asp">Payments</a>
+    <a href="default.asp">COmpatibility Finder</a>
     </>
   )
 }
