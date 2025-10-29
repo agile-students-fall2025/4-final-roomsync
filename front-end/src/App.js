15cf1a8f18f94b9922b726e0772a86b073aaf2fa
eselmsenavarank <varankeslemsena@gmail.com>
2025-10-29 17:51:01 -0400

    implemented the invite roomate/create house page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,7 @@ import Chores from './Chores';
 import AddChore from './AddChore';
 import Header from './Header';
 import Footer from './Footer';
+import CreateHome from './CreateHome'
 
 const App = props => {
   return (
@@ -20,6 +21,8 @@ const App = props => {
             <Route path="/chores" element={<Chores />} />
             {/* a route for the add chore page */}
             <Route path="/chores/add" element={<AddChore />} />
+            {/* a route for the invite roommate to create a new home */}
+            <Route path="/create" element={<CreateHome />} /> {/* This page won't be in the navbar, and have access from onboarding or in home page*/}
 
           </Routes>
         </main>
