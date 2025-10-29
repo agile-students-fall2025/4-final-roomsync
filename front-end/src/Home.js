a0baf15313e13ede28a27d0fd1ff6c4dfb22931b
eselmsenavarank <varankeslemsena@gmail.com>
2025-10-29 18:42:18 -0400

    listed users statistic for the home page like chores

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 import './Home.css'
 
 /**
@@ -7,17 +8,38 @@ import './Home.css'
  * @returns The contents of this component, in JSX form.
  */
 const Home = props => {
+  //Temperary data from chores page
+    const [chores, setChores] = useState([
+      { id: 1, name: 'Clean Kitchen', assignedTo: 'Brian', finished: false },
+      { id: 2, name: 'Take Out Trash', assignedTo: 'Eslem', finished: false },
+      { id: 3, name: 'Vacuum Living Room', assignedTo: 'Jacod', finished: true }
+    ])
+
+    //TODO : this filteres accoring to the user id (use Brian for sprint 1)
+    const userID = 'Brian'; // this will be userId in the backend
+    const filteredChores = chores.filter(chore => chore.assignedTo == userID && chore.finished == false);
+
   return (
     <>
     <div class="">
-    <form action="get">
-    <div class="statistics-home">
-     <h3>Upcoming Chorses</h3>
-     <ul id="chorses"></ul>
-     <h3>Upcoming Payments</h3>
-     <ul id="payments"></ul>
-     </div>
-    </form>
+      {/*TODO form will get the info from backend*/}
+      <form action="get">
+        <div class="statistics-home">
+          <div class="stats">
+            <h3>Your Upcoming Chorses</h3>
+            <ul id="chorses">
+              {filteredChores.map(chore => (
+                <li key={chores.id}>{chore.name}</li>
+              ))}
+            </ul>
+          </div>
+          <div class="stats">
+            <h3>Your Upcoming Payments</h3>
+            <ul id="payments"></ul>
+          </div>
+          
+        </div>
+      </form>
     </div>
 
     <div class="buttons-home">
