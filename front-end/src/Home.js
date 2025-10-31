0338f246c849df0b7f4c09de63321b0abb51701c
eselmsenavarank <varankeslemsena@gmail.com>
2025-10-31 16:57:28 -0400

    edited the css styling

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -15,14 +15,27 @@ const Home = props => {
       { id: 3, name: 'Vacuum Living Room', assignedTo: 'Jacod', finished: true }
     ])
 
+    const [payments, setPayments] = useState([
+    { id: 1, name: "Pay electricity bill", amount: 120.5, createdAt: "2025-10-20", cleared: false, categoryId: 1, paidBy: 1, owedBy: [1, 2, 3, 4, 5] },
+    { id: 2, name: "Refill water filter", amount: 35.0,  createdAt: "2025-10-18", cleared: true,  categoryId: 1, paidBy: 2, owedBy: [1, 2, 3] },
+    { id: 3, name: "Buy milk",            amount: 4.25,  createdAt: "2025-10-25", cleared: true,  categoryId: 2, paidBy: 3, owedBy: [1, 3, 4] },
+    { id: 4, name: "Buy vegetables",      amount: 18.6,  createdAt: "2025-10-26", cleared: false, categoryId: 2, paidBy: 1, owedBy: [1, 2, 5] },
+    { id: 5, name: "Restock snacks",      amount: 22.4,  createdAt: "2025-10-23", cleared: false, categoryId: 2, paidBy: 4, owedBy: [1, 2, 3, 4] },
+    { id: 6, name: "Change air filter",   amount: 15.0,  createdAt: "2025-10-21", cleared: false, categoryId: 3, paidBy: 5, owedBy: [1, 2, 3, 4, 5] },
+    { id: 7, name: "Check smoke detector",amount: 10.0,  createdAt: "2025-10-19", cleared: true,  categoryId: 3, paidBy: 2, owedBy: [2, 3, 5] },
+  ]);
+
     //TODO : this filteres accoring to the user id (use Brian for sprint 1)
-    const userID = 'Brian'; // this will be userId in the backend
-    const filteredChores = chores.filter(chore => chore.assignedTo == userID && chore.finished == false);
+    const userID = 1; // this will be userId in the backend
+    
+    const filteredChores = chores.filter(chore => chore.id == userID && chore.finished == false && chore.assignedTo);
+    const user = chores.find(user => user.id = userID )
 
   return (
     <>
     <div class="">
       {/*TODO form will get the info from backend*/}
+      <h2>Welcome {user.assignedTo}</h2>
       <form action="get">
         <div class="statistics-home">
           <div class="stats">
@@ -43,10 +56,10 @@ const Home = props => {
     </div>
 
     <div class="buttons-home">
-      <a href="default.asp">Skill Swap</a>
-      <a href="/chores">Chores</a>
-      <a href="default.asp">Payments</a>
-      <a href="default.asp">Compatibility Finder</a>
+      <a class="button-5" href="default.asp">Skill Swap</a>
+      <a class="button-5" href="/chores">Chores</a>
+      <a class="button-5" href="default.asp">Payments</a>
+      <a class="button-5" href="default.asp">Compatibility Finder</a>
     </div>
     </>
   )
