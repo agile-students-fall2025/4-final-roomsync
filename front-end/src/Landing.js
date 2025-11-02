7eb4d4d5a2736b015c0341ee95c3cea1cbf33dcb
eselmsenavarank <varankeslemsena@gmail.com>
2025-11-01 22:04:13 -0400

    edited the website design

--- front-end/src/Landing.js
+++ front-end/src/Landing.js
@@ -1,23 +1,25 @@
 import { useEffect, useState } from 'react'
+import './Landing.css'; // Create this CSS file
 
 export default function Landing() {
-  
-
   return (
     <>
-     <div class="intro">
-     <h1>Welcome to RoomSync</h1>
-     <h3>Living with roommates can be a rewarding experience, but it often comes with significant friction. RoomSync is here for you:</h3>
-     <ul>
-        <li>Chore and expense tracking</li>
-        <li>Finding a compatible roommate</li>
-        <li>Building a genuine community</li>
-     </ul>
+    <head></head>
+    <div className="landing-container">
+      <div className="intro">
+        <h1>Welcome to RoomSync</h1>
+        <h3>Living with roommates can be a rewarding experience, but it often comes with significant friction. RoomSync is here for you:</h3>
+        <ul>
+          <li>Chore and expense tracking</li>
+          <li>Finding a compatible roommate</li>
+          <li>Building a genuine community</li>
+        </ul>
+      </div>
+      <div className="auth-buttons">
+        <a href="/login" className="btn btn-primary">Sign In</a>
+        <a href="/register" className="btn btn-secondary">Register</a>
+      </div>
     </div>
-    <a href="/login">Sign In</a>
-    <a href="/register">Register</a>
     </>
   )
 }
-
-
