edbc507d1a9def7499c4c494e5be1dbf3dae5b1b
shrimpforfree <brian920922@gmail.com>
2025-11-14 12:53:01 -0500

    chore test

--- back-end/test/test.js
+++ back-end/test/test.js
@@ -93,3 +93,118 @@ describe('POST /api/users/:userId/assign-room', () => {
       })
   })
 })
+
+// Chores routes tests
+describe('GET /api/rooms/:roomId/chores', () => {
+  it('should return all chores for a room', done => {
+    request
+      .execute(app)
+      .get('/api/rooms/1/chores')
+      .end((err, res) => {
+        expect(res).to.have.status(200)
+        expect(res.body).to.be.an('array')
+        expect(res.body.length).to.be.greaterThan(0)
+        expect(res.body[0]).to.have.property('id')
+        expect(res.body[0]).to.have.property('name')
+        expect(res.body[0]).to.have.property('assignedTo')
+        expect(res.body[0]).to.have.property('finished')
+        expect(res.body[0]).to.have.property('roomId')
+        done()
+      })
+  })
+})
+
+describe('GET /api/rooms/:roomId/chores/:id', () => {
+  it('should return a specific chore', done => {
+    request
+      .execute(app)
+      .get('/api/rooms/1/chores/1')
+      .end((err, res) => {
+        expect(res).to.have.status(200)
+        expect(res.body).to.have.property('id').that.equals(1)
+        expect(res.body).to.have.property('name')
+        expect(res.body).to.have.property('assignedTo')
+        expect(res.body).to.have.property('finished')
+        expect(res.body).to.have.property('roomId')
+        done()
+      })
+  })
+
+  it('should return 404 for non-existing chore', done => {
+    request
+      .execute(app)
+      .get('/api/rooms/1/chores/9999')
+      .end((err, res) => {
+        expect(res).to.have.status(404)
+        expect(res.body).to.have.property('message')
+        done()
+      })
+  })
+})
+
+describe('POST /api/rooms/:roomId/chores', () => {
+  it('should create a new chore', done => {
+    const newChore = {
+      name: 'Test Chore',
+      assignedTo: 1
+    }
+    request
+      .execute(app)
+      .post('/api/rooms/1/chores')
+      .send(newChore)
+      .end((err, res) => {
+        expect(res).to.have.status(200)
+        expect(res.body).to.have.property('id')
+        expect(res.body).to.have.property('name').that.equals(newChore.name)
+        expect(res.body).to.have.property('assignedTo').that.equals(newChore.assignedTo)
+        expect(res.body).to.have.property('finished').that.equals(false)
+        expect(res.body).to.have.property('roomId').that.equals(1)
+        done()
+      })
+  })
+})
+
+describe('PUT /api/rooms/:roomId/chores/:id', () => {
+  it('should update a chore', done => {
+    const updates = {
+      name: 'Updated Chore Name',
+      finished: true
+    }
+    request
+      .execute(app)
+      .put('/api/rooms/1/chores/1')
+      .send(updates)
+      .end((err, res) => {
+        expect(res).to.have.status(200)
+        expect(res.body).to.have.property('id').that.equals(1)
+        expect(res.body).to.have.property('name').that.equals(updates.name)
+        expect(res.body).to.have.property('finished').that.equals(true)
+        done()
+      })
+  })
+
+  it('should toggle finished status', done => {
+    request
+      .execute(app)
+      .put('/api/rooms/1/chores/2')
+      .send({ finished: true })
+      .end((err, res) => {
+        expect(res).to.have.status(200)
+        expect(res.body).to.have.property('finished').that.equals(true)
+        done()
+      })
+  })
+})
+
+describe('DELETE /api/rooms/:roomId/chores/:id', () => {
+  it('should delete a chore', done => {
+    request
+      .execute(app)
+      .delete('/api/rooms/1/chores/3')
+      .end((err, res) => {
+        expect(res).to.have.status(200)
+        expect(res.body).to.have.property('success').that.equals(true)
+        done()
+      })
+  })
+})
