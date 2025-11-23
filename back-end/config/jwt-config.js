c0cab141934f9c9d5e9faa6dd63b5607bb18cb1b
eselmsenavarank <varankeslemsena@gmail.com>
2025-11-23 15:25:33 -0500

    cookie routes and jwt configs

---
+++ back-end/config/jwt-config.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+const ObjectId = mongoose.Types.ObjectId
+import User from '../models/User.js'
+
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
+
+// set up some JWT authentication options for passport
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
+  secretOrKey: process.env.JWT_SECRET, 
+}
+// console.log(jwtOptions) // debug to make sure the secret from the .env file is loaded correctly
+
+const jwtVerifyToken = async function (jwt_payload, next) {
+  console.log('JWT payload received', jwt_payload) // debugging
+
+  // check if the token has expired
+  const expirationDate = new Date(jwt_payload.exp * 1000) // convert from seconds to milliseconds
+  if (expirationDate < new Date()) {
+    return next(null, false, { message: 'JWT token has expired.' })
+  }
+
+  try {
+    // find this user in the database
+    const userId = new ObjectId(jwt_payload.id) // convert the string id to an ObjectId
+    const user = await User.findOne({ _id: userId }).exec()
+    if (user) {
+      next(null, user)
+    } else {
+      // we didn't find the user... fail!
+      next(null, false, { message: 'User not found' })
+    }
+  } catch (error) {
+    console.error('Error finding user:', error)
+    next(error, false)
+  }
+}
+
+// Create and export the strategy
+const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerifyToken)
+
+export default jwtStrategy
