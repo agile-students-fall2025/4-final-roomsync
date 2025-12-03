e2d67f8f31f2c81f569a19ab5c6977445ad156ae
ginny1536 <ginny1536@outlook.com>
2025-12-03 00:00:56 -0500

    added apartment profile schema

---
+++ back-end/models/Compatibility/Apartment.js
@@ -0,0 +1,71 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+
+const ApartmentSchema = new Schema({
+  listingTitle: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  monthlyRent: {
+    type: Number,
+    required: true,
+  },
+  deposit: {
+    type: Number,
+    required: false,
+    default: null,
+  },
+  startDate: {
+    type: Date,
+    required: false,
+    default: Date.now,
+  },
+  endDate: {
+    type: Date,
+    required: false,
+    default: null,
+  },
+  isPrivate: {
+    type: Boolean,
+    required: true,
+  },
+  amenities: {
+    type: [String],
+    enum: ['pet-friendly', 'in-unit-laudry', 'air-conditioning', 'gym', 'elevator', 'furnished', 'doorman'],
+    required: false,
+    default: [],
+  },
+  houseRules: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  idealRoommate: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  images: {
+    type: [String],
+    required: false,
+    default: [],
+    validate: {
+      validator: function(v) {
+        return v.length <= 10;  // Max 10 images
+      },
+      message: 'No more than 10 images'
+    }
+  }
+}, {
+  timestamps: true
+})
+
+const Apartment = mongoose.model('Apartment', ApartmentSchema)
+
+export default Apartment
