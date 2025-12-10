52f3ce520edcf6ed58224d5c2ae229194cfd257f
eselmsenavarank <varankeslemsena@gmail.com>
2025-12-10 08:39:17 -0500

    calender-improv

--- front-end/src/EventCalendar.js
+++ front-end/src/EventCalendar.js
@@ -152,7 +152,7 @@ export default function EventCalendar() {
                   <ul>
                     {selectedDateEvents.map(event => (
                       <li key={event.id} className="event-item">
-                        <strong>{event.name}</strong>
+                        <strong>{event.name} | {event.time} | {event.location}</strong>
                         <p className="muted small">{event.description}</p>
                       </li>
                     ))}
